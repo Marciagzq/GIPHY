2c6658bfa9f5b06c63a62a8d075a3a1507cd04bb
Marcia Zegarra <marcia.gzq@gmail.com>
2019-07-17 22:52:56 -0400

    Add description on README file. add coments on my code to be easier to read. Add the last details for 'Responsiveness' website.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GIPHY
+
+The code in this folder is an assignment for UCF bootcamp. 
+
+On this code, we were challenged to use APIs and call certain information defined by parameters,
+This web application will allow the user to get some gifs and certain details of itself with it.
+The user also may be able to add some new buttons with the animal, which is going to define the search with every click.
+If you click a different button (animal), the gifs section will remove the previous search and it will show the new information. 
+
+If any suggestion, do not hesitate to contact me on my personal email: marcia.gzq@gmail.com
+I would appreciate any comment, I know it may be very beneficial on my proffesional growth. 
+
+
+
--- logic.js
+++ logic.js
@@ -1,107 +1,109 @@
-
-var topics = ['cat', 'chameleon', 'ferret', 'turtle', 'pig', 'rabit','crocodile', 'monkey', 'bambi', 'dog', 'hamster', 'goat', 'pig', 'parrot'];
+// Global arrays and variables defined to call them later;
+// Array of strings we will define as buttons later.
+var topics = ['cat', 'chameleon', 'dolphin', 'ferret', 'turtle', 'pig', 'rabit', 'crocodile', 'monkey', 'dog', 'hamster', 'goat', 'chicken', 'parrot', 'fish', 'gorila', 'cow'];
 var newtopicss = "";
 var currentsearch = []
 var previoustopic = ""
 var offset = 0;
 
-function selectedButton(animal, limitNumber){
+function selectedButton(animal, limitNumber) {
 
+    //This code asures the user to grab 10 items more on each click to the same,
     var add = false;
-    if(previoustopic == animal){
+    if (previoustopic == animal) {
         offset += 10
     }
-    else{
-        $( ".searchresult" ).remove();
+    else {
+        $(".searchresult").remove();
         previoustopic = animal;
         offset = 0
-    } 
-    
-    // this function would grab the information thru the API
-    // It has the parameters defined 
-    var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=WmLVICCu0jxWYPKpqx7o5w3ubZu2Wbp1&limit=" + limitNumber + "&offset="+ offset);
+    }
+
+    // this function would grab the information from an existing API
+    // It has the parameters defined to search for an specific information.
+    var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=WmLVICCu0jxWYPKpqx7o5w3ubZu2Wbp1&limit=" + limitNumber + "&offset=" + offset);
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
-       console.log(response);
- 
+    }).then(function (response) {
+        console.log(response);
+
         previoustopic = animal;
         var responselist = response.data;
         currentsearch = responselist;
 
-        for (var i = 0; i < responselist.length; i++){
+        for (var i = 0; i < responselist.length; i++) {
             responselist[i].still = true;
             var rating = (responselist[i].rating);
             var title = responselist[i].title;
             var mp4 = (responselist[i].images.downsized_still.url);
-            var gifCont = $("<div listindex='"+i+"' still='true' class='searchresult'><h5 class='rating'>"+ rating +"</h5><img width='100px' height='100px' class='mp4' src='"+mp4+"'><p class='rating' >"+ title +"</p></div>")
-            $("#gif-container").append(gifCont);    
+            var gifCont = $("<div listindex='" + i + "' still='true' class='searchresult fill'><h5 class='rating'>" + rating + "</h5><img width='200px' height='200px' class='mp4' draggable='true' src='" + mp4 + "'><p class='rating' >" + title + "</p></div>")
+            $("#gif-container").append(gifCont);
         }
-    });
 
+    });
 };
 
-function newButton(value){
+// This function will create a new button append to our original array of buttons
+// also will avoid the user to repeat the animal
+function newButton(value) {
     var found = false;
-    for (var i = 0; i < topics.length; i++){
-        if (topics[i] === value){
+    for (var i = 0; i < topics.length; i++) {
+        if (topics[i] === value) {
             found = true;
-        } 
+        }
     }
-
-    if(found){        
+    if (found) {
         alert("Repeated Animal");
     }
-    else{
+    else if (value == false) {
+        alert("Oops! you should add an Animal");
+    }
+    else {
         topics.push(value);
-        var addedButton = $("<input type='button' class='defined-button' value='" + value +"'></input>");
-        $(".col-lg-12").append(addedButton);
+        var addedButton = $("<input type='button'class='defined-button' value='" + value + "'></input>");
+        $(".col-sm-12").append(addedButton);
     }
 }
 
-
 //
-$( document ).ready(function() {
-    
-    $('.col-lg-12').on('click', '.defined-button', function() {
+$(document).ready(function () {
+
+    $('.col-sm-12').on('click', '.defined-button', function () {
         var button = $(this).val();
         selectedButton(button, 10);
     });
-    
-    $('#gif-container').on('click', '.searchresult', function() {
+
+    $('#gif-container').on('click', '.searchresult', function () {
 
         var index = $(this).attr("listindex");
         var newurl = currentsearch[index].images.preview_gif.url;
         var oldurl = currentsearch[index].images.downsized_still.url;
         var still = currentsearch[index].still;
-        if(still){
+        if (still) {
             $(this).children("img").attr("src", newurl)
             currentsearch[index].still = false
         }
-        else{
+        else {
             $(this).children("img").attr("src", oldurl)
             currentsearch[index].still = true
         }
     });
 
-
-   for (var i = 0; i < topics.length; i++){
+    //I created this function to add and append the new buttons 
+    for (var i = 0; i < topics.length; i++) {
         var temp = topics[i];
-        
-        var button = $("<input type='button' class='defined-button' value='" + temp +"'></input>");
-        $(".col-lg-12").append(button);
-   }
 
-    $("#submit-button").click(function(){
-       var inputVal = $("#button-added").val();
+        var button = $("<input type='button' class='defined-button' value='" + temp + "'></input>");
+        $(".col-sm-12").append(button).addClass('defined-button-new');
+    }
+
+    $("#submit-button").click(function () {
+        var inputVal = $("#button-added").val();
         newButton(inputVal);
-      
     });
-
-    
 });
 
 
-  
+
 
